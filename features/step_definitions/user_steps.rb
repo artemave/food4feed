7d8d1f22564bd0ffdf7465994afa1ce8b4a7f9cb
artemave <artemave@gmail.com>
2010-03-23 16:29:48 +0000

    login step passes

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,5 +1,11 @@
 Given /^I am logged in$/ do
   Factory(:user)
-  u = User.authenticate('artemave@gmail.com', 'secret')
-  raise "authentication failed" if u.nil?
+  #u = User.authenticate('artemave@gmail.com', 'secret')
+  #raise "authentication failed" if u.nil?
+  visit path_to('the homepage')
+  click_link 'Log In'
+  fill_in 'login', :with => 'artemave@gmail.com'
+  fill_in 'password', :with => 'secret'
+  click_button 'Log In'
+  Then "I should see \"You have logged in\""
 end
